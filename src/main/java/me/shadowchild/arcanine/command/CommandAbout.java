67793e4e88d77bab41f53e70478b14a0d39c7236
ShadowChild <shadychild12@gmail.com>
2020-06-08 22:15:51 +0100

    Added about command, next up, functional commands

---
+++ src/main/java/me/shadowchild/arcanine/command/CommandAbout.java
@@ -0,0 +1,38 @@
+package me.shadowchild.arcanine.command;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+
+import java.awt.*;
+
+public class CommandAbout extends AbstractCommand {
+
+
+    @Override
+    public void onMessage(MessageReceivedEvent event, MessageChannel channel, User sender, String alias) {
+
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle("About Arcanine", "https://arcanine.shadowchild.me");
+        builder.setImage("http://arcanine.shadowchild.me/pokemon-arcanine.jpg");
+        builder.setThumbnail("https://arcanine.shadowchild.me/Dali.jpg");
+        builder.setColor(Color.decode("#E91E63"));
+        builder.setDescription("Info and Stats about Arcanine");
+        builder.setAuthor("ShadowChild", "https://shadowchild.me");
+        {
+            builder.setTimestamp(event.getMessage().getTimeCreated());
+            builder.addField("Version", "SNAPSHOT", true);
+            builder.addField("Java Version", System.getProperty("java.version"), true);
+        }
+        MessageEmbed embed = builder.build();
+        channel.sendMessage(embed).queue();
+    }
+
+    @Override
+    public void onReactionAdd(MessageReactionAddEvent event, MessageChannel channel, User sender) {
+
+    }
+}
